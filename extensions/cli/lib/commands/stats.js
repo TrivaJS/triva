b1c9f22a4af117dc6e8b4c4d4f9ec4f136f9190a
Kris Powers <85710701+KrisPowers@users.noreply.github.com>
2026-05-10 15:20:07 -0400

    CodeQuality - Resolve extensions/cli/lib/commands/stats.js:12

--- extensions/cli/lib/commands/stats.js
+++ extensions/cli/lib/commands/stats.js
@@ -9,7 +9,7 @@ export class StatsCommands extends Command {
   static async handle(subcommand, options) {
     const config = await this.loadConfig(options.config);
 
-    const { build, log } = await import('triva');
+    const { build } = await import('triva');
     await build(config);
 
     switch (subcommand) {
